acda25c44cb1e3273ab21e0112b208fc7c89e816
Brayan Elian Jaimes <brepenaj@udistrital.edu.co>
2024-02-22 18:22:08 -0500

    add change

--- demo/src/main/java/com/demo/demo/Security/SecurityConfig.java
+++ demo/src/main/java/com/demo/demo/Security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.demo.demo.Security;
  import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -40,21 +40,17 @@ public class SecurityConfig {
         @Bean
         SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
             http
-                    .csrf().disable()
-                    .exceptionHandling()
-                    .authenticationEntryPoint(jwtAuthenticationEntryPoint) 
-                    .and()
-                    .sessionManagement() 
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .authorizeHttpRequests() 
-                    .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                    .requestMatchers("/auth/**").permitAll()
-                    .requestMatchers("/api/init").permitAll()
-                    .requestMatchers("/api/users/**").hasAuthority("ADMIN")
-                    .anyRequest().authenticated()
-                    .and()
-                    .httpBasic();
+                    .csrf(csrf -> csrf.disable())
+                    .exceptionHandling(handling -> handling
+                            .authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                    .sessionManagement(management -> management
+                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authorizeHttpRequests(requests -> requests
+                            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                            .requestMatchers("/auth/**").permitAll()
+                            .requestMatchers("/api/init").permitAll()
+                            .requestMatchers("/api/users/**").hasAuthority("ADMIN")
+                            .anyRequest().authenticated());
             http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
             return http.build();
         }
--- demo/src/main/java/com/demo/demo/Service/UserService.java
+++ demo/src/main/java/com/demo/demo/Service/UserService.java
@@ -17,7 +17,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Service
 public class UserService {
